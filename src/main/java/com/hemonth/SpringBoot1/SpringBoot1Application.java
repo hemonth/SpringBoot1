dd488495ec747fb149c84e94d7fd92758fc4761d
Hemonth.Mandava <Hemonth.Mandava@SSTECHLAPTOP365.sstech.internal>
2017-07-09 12:00:56 -0500

    did fix imports

--- src/main/java/com/hemonth/SpringBoot1/SpringBoot1Application.java
+++ src/main/java/com/hemonth/SpringBoot1/SpringBoot1Application.java
@@ -1,9 +1,7 @@
 package com.hemonth.SpringBoot1;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
